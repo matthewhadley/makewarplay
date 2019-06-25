822e9761fb8112810568e42285bc0df4152a94d8
matthewhadley <matthew.hadley@gmail.com>
2019-06-25 13:21:46 -0700

    reset

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+markup=""
+
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    markup="${markup}<li><a href=\"https://instagram.com/p/${line}\"><img src=\"https://instagram.com/p/${line}/media/?size=l\" /></a></li>"
+done < "./ig.txt"
+
+sedi () {
+  sed --version >/dev/null 2>&1 && sed -i -- "$@" || sed -i "" "$@"
+}
+
+sedi "s%<!--ig:start-->.*<!--ig:end-->%<!--ig:start-->${markup}<!--ig:end-->%" ./www/index.html

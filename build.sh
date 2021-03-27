659f1f176992e40da205fe2130e048be2966e85d
matthewhadley <matthew.hadley@gmail.com>
2021-03-27 14:36:31 -0500

    remove ig

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-markup=""
+# markup=""
 
-while IFS='' read -r line || [[ -n "$line" ]]; do
-    markup="${markup}<li><a href=\"https://instagram.com/p/${line}\"><img src=\"https://instagram.com/p/${line}/media/?size=l\" /></a></li>"
-done < "./ig.txt"
+# while IFS='' read -r line || [[ -n "$line" ]]; do
+#     markup="${markup}<li><a href=\"https://instagram.com/p/${line}\"><img src=\"https://instagram.com/p/${line}/media/?size=l\" /></a></li>"
+# done < "./ig.txt"
 
-sedi () {
-  sed --version >/dev/null 2>&1 && sed -i -- "$@" || sed -i "" "$@"
-}
+# sedi () {
+#   sed --version >/dev/null 2>&1 && sed -i -- "$@" || sed -i "" "$@"
+# }
 
-sedi "s%<!--ig:start-->.*<!--ig:end-->%<!--ig:start-->${markup}<!--ig:end-->%" ./www/index.html
+# sedi "s%<!--ig:start-->.*<!--ig:end-->%<!--ig:start-->${markup}<!--ig:end-->%" ./www/index.html
